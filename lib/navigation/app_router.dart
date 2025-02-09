0a4924716379f2ad27d36f80a29e98c17925cb8c
24bartixx <jestgitbartix@gmail.com>
2025-02-09 18:05:31 +0100

    init: navigation set up

---
+++ lib/navigation/app_router.dart
@@ -0,0 +1,13 @@
+import "package:auto_route/auto_route.dart";
+
+part "app_router.gr.dart";
+
+@AutoRouterConfig(replaceInRouteName: "Screen,Route")
+class AppRouter extends RootStackRouter {
+  
+  @override
+  List<AutoRoute> get routes => [
+
+  ];
+
+}
