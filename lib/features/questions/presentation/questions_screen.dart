a05163d12e6c820011577abe201a03a5e1d07c5c
24bartixx <jestgitbartix@gmail.com>
2025-02-11 22:59:57 +0100

    chore: forms validators to prevent empty fields

--- lib/features/questions/presentation/questions_screen.dart
+++ lib/features/questions/presentation/questions_screen.dart
@@ -1,6 +1,7 @@
 import "package:auto_route/auto_route.dart";
 import "package:flutter/material.dart";
 import "package:flutter_form_builder/flutter_form_builder.dart";
+import "package:form_builder_validators/form_builder_validators.dart";
 import "package:sleep_app/constants/app_dimensions.dart";
 import "package:sleep_app/extensions/context_extensions.dart";
 import "package:sleep_app/navigation/app_router.dart";
@@ -12,92 +13,133 @@ class QuestionsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final formKey = GlobalKey<FormBuilderState>();
+
     return Scaffold(
       appBar: AppBar(title: Text("Questions")),
       body: SingleChildScrollView(
         padding: EdgeInsets.symmetric(horizontal: AppDimensions.paddingBig),
         child: FormBuilder(
+          key: formKey,
+          autovalidateMode: AutovalidateMode.disabled,
           child: Column(
             children: [
+
               SizedBox(height: AppDimensions.heightBig),
               Text(context.localize.questions_headline, style: context.theme.textTheme.headlineMedium?.copyWith(fontWeight: FontWeight.normal)),
               SizedBox(height: AppDimensions.heightBig),
               Divider(color: light, thickness: 1,),
               SizedBox(height: AppDimensions.heightBig),
+
               FormBuilderDateTimePicker(
                 name: "bedtime",
                 inputType: InputType.time,
                 initialTime: TimeOfDay(hour: 0, minute: 0),
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(errorText: "This field is required"),
+                ]),
                 decoration: InputDecoration(
                   labelText: context.localize.go_to_bed_time,
                   floatingLabelBehavior: FloatingLabelBehavior.always,
                   icon: Icon(Icons.bed, color: light),
                 ),
               ),
+
               SizedBox(height: AppDimensions.heightHuge),
+
               FormBuilderDateTimePicker(
                 name: "go_sleep_time",
                 inputType: InputType.time,
+                initialTime: TimeOfDay(hour: 0, minute: 0),
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(errorText: "This field is required"),
+                ]),
                 decoration: InputDecoration(
                   labelText: context.localize.go_sleep_time,
                   floatingLabelBehavior: FloatingLabelBehavior.always,
                   icon: Icon(Icons.bedtime, color: light),
                 ),
-                initialTime: TimeOfDay(hour: 0, minute: 0),
               ),
+
               SizedBox(height: AppDimensions.heightHuge),
-              FormBuilderDateTimePicker(
+
+              FormBuilderTextField(
                 name: "fall_asleep_time",
-                inputType: InputType.time,
+                keyboardType: TextInputType.number,
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(errorText: "This field is required"),
+                ]),
                 decoration: InputDecoration(
                   labelText: context.localize.fall_asleep_time,
                   floatingLabelBehavior: FloatingLabelBehavior.always,
                   icon: Icon(Icons.bedtime, color: light),
                 ),
-                initialTime: TimeOfDay(hour: 0, minute: 0),
               ),
+
               SizedBox(height: AppDimensions.heightHuge),
+
               FormBuilderTextField(
                 name: "awekenings_times",
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(errorText: "This field is required"),
+                ]),
                 decoration: InputDecoration(
                   labelText: context.localize.awakening_times,
                   floatingLabelBehavior: FloatingLabelBehavior.always,
                   icon: Icon(Icons.alarm, color: light),
                 ),
               ),
+
               SizedBox(height: AppDimensions.heightHuge),
+
               FormBuilderTextField(
                 name: "awakening_time_total",
+                keyboardType: TextInputType.number,
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(errorText: "This field is required"),
+                ]),
                 decoration: InputDecoration(
                   labelText: context.localize.awakening_time_total,
                   floatingLabelBehavior: FloatingLabelBehavior.always,
                   icon: Icon(Icons.timer, color: light),
                 ),
-                keyboardType: TextInputType.number,
               ),
+
               SizedBox(height: AppDimensions.heightHuge),
+
               FormBuilderDateTimePicker(
                 name: "wakeup_time",
                 inputType: InputType.time,
                 initialTime: TimeOfDay(hour: 8, minute: 0),
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(errorText: "This field is required"),
+                ]),
                 decoration: InputDecoration(
                   labelText: context.localize.wake_up_time,
                   floatingLabelBehavior: FloatingLabelBehavior.always,
                   icon: Icon(Icons.sunny, color: light),
                 ),
               ),
+
               SizedBox(height: AppDimensions.heightHuge),
+
               FormBuilderDateTimePicker(
                 name: "leave_time",
                 inputType: InputType.time,
                 initialTime: TimeOfDay(hour: 8, minute: 0),
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(errorText: "This field is required"),
+                ]),
                 decoration: InputDecoration(
                   labelText: context.localize.leave_bed_time,
                   floatingLabelBehavior: FloatingLabelBehavior.always,
                   icon: Icon(Icons.man, color: light),
                 ),
               ),
+
               SizedBox(height: AppDimensions.heightHuge),
+
               SliderTheme(
                 data: SliderTheme.of(context).copyWith(
                   tickMarkShape: RoundSliderTickMarkShape(tickMarkRadius: 0),
@@ -115,6 +157,9 @@ class QuestionsScreen extends StatelessWidget {
                   minValueWidget: (min) => SizedBox(),
                   valueWidget: (value) => Text("$value / 5"),
                   maxValueWidget: (max) => SizedBox(),
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(errorText: "This field is required"),
+                  ]),
                   decoration: InputDecoration(
                     labelText: context.localize.sleep_rate,
                     floatingLabelBehavior: FloatingLabelBehavior.always,
@@ -122,14 +167,16 @@ class QuestionsScreen extends StatelessWidget {
                   ),
                 ),
               ),
+
               SizedBox(height: AppDimensions.heightHuge),
               Divider(color: light, thickness: 1,),
               SizedBox(height: AppDimensions.heightMedium),
+
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   TextButton(
-                    onPressed: () => (),
+                    onPressed: () => context.router.popForced(),
                     style: context.theme.textButtonTheme.style?.copyWith(
                       foregroundColor: WidgetStateProperty.all(amethyst),
                       backgroundColor: WidgetStateProperty.all(Colors.transparent),
@@ -140,10 +187,16 @@ class QuestionsScreen extends StatelessWidget {
                   SizedBox(width: AppDimensions.heightMedium,), 
                   TextButton(
                     child: Text(context.localize.submit),
-                    onPressed: () => context.router.push(const ThankYouRoute()),
+                    onPressed: () {
+                      if(formKey.currentState?.saveAndValidate(focusOnInvalid: false) ?? false) {
+                        // TODO save form results to backend
+                        context.router.push(ThankYouRoute());
+                      }
+                    },
                   ),
                 ],
               ),
+
               SizedBox(height: AppDimensions.heightHuge),
               SizedBox(height: AppDimensions.heightHuge),
               SizedBox(height: AppDimensions.heightHuge),
