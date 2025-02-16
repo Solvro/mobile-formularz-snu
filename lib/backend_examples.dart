6326ab5b5f06a21f5b4e8f867da7e4a9dcb60851
Szymon Kowaliński <szymon@kowalinski.dev>
2025-02-17 00:31:18 +0100

    feat: add example usage of the backend operations

---
+++ lib/backend_examples.dart
@@ -0,0 +1,31 @@
+import "package:formularz_snu_client/formularz_snu_client.dart";
+import "package:serverpod_flutter/serverpod_flutter.dart";
+
+// for now we don't have any auth nor api key
+final client = Client("https://api.formularz-snu.s.solvro.pl/")
+  ..connectivityMonitor = FlutterConnectivityMonitor();
+
+Future<void> exampleQueries() async {
+  await client.participant.doesThisEmailExist(
+    "test@test.test", // returns whether a person is enrolled in the study
+  );
+  await client.config
+      .isStudyInProgress(); // returns whether the study is in progress (current datetime is after start date and before end date)
+
+  await client.formEntry.submitForm(
+    // submitting a form
+    FormData(
+      inBedStartTime: DateTime.now(),
+      fallingAsleepTime: DateTime.now(),
+      wakeUpTime: DateTime.now(),
+      midNightAwaikingsCount: 6,
+      outBedTime: DateTime.now(),
+      totalMidNightAwaikingsTime: Duration(days: 1),
+    ),
+    "test@test.test",
+  );
+
+  await client.formEntry.hasTodayAlreadySentResponse(
+    "test@test.test", // returns whether the participant has already submitted a form today
+  );
+}
