8f4358fe411bebfcc6bfe6eda61e85100c2c346d
24bartixx <jestgitbartix@gmail.com>
2025-02-19 01:06:44 +0100

    feat: native splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 import "package:alarm/alarm.dart";
 import "package:flutter/material.dart";
 import "package:flutter_localizations/flutter_localizations.dart";
+import "package:flutter_native_splash/flutter_native_splash.dart";
 import "package:sleep_app/l10n/app_localizations.dart";
 import "package:sleep_app/navigation/app_router.dart";
 import "package:sleep_app/theme/theme_data.dart";
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(
+    widgetsBinding: WidgetsFlutterBinding.ensureInitialized(),
+  );
+
   await Alarm.init();
   final appRouter = AppRouter();
+
+  FlutterNativeSplash.remove();
+
   runApp(MyApp(router: appRouter));
 }
 
