f10d3da5476612859ac7c0306921f2277d1f0cd5
24bartixx <jestgitbartix@gmail.com>
2025-02-19 19:56:23 +0100

    chore: format

--- lib/features/alarm/presentation/alarm_screen.dart
+++ lib/features/alarm/presentation/alarm_screen.dart
@@ -11,7 +11,6 @@ import "package:sleep_app/theme/app_colors.dart";
 
 @RoutePage()
 class AlarmScreen extends HookWidget {
-
   const AlarmScreen({super.key});
 
   @override
@@ -20,18 +19,23 @@ class AlarmScreen extends HookWidget {
     final selectedTime = useState(const TimeOfDay(hour: 8, minute: 0));
     final isAlarmEnabled = useState(false);
 
-    useEffect(() {
-      Future.microtask(() async {
-        final alarmSettings = await alarmCacheRepository.loadAlarmSettings();
-        isAlarmEnabled.value = alarmSettings.isEnabled;
-        selectedTime.value = alarmSettings.time;
-      });
-      return null;
-    }, [],);
+    useEffect(
+      () {
+        Future.microtask(() async {
+          final alarmSettings = await alarmCacheRepository.loadAlarmSettings();
+          isAlarmEnabled.value = alarmSettings.isEnabled;
+          selectedTime.value = alarmSettings.time;
+        });
+        return null;
+      },
+      [],
+    );
 
     Future<void> saveAlarmSettings() async {
-      final alarmSettings =
-          AlarmSettings(isEnabled: isAlarmEnabled.value, time: selectedTime.value);
+      final alarmSettings = AlarmSettings(
+        isEnabled: isAlarmEnabled.value,
+        time: selectedTime.value,
+      );
       await alarmCacheRepository.saveAlarmSettings(alarmSettings);
 
       if (isAlarmEnabled.value) {
@@ -108,7 +112,8 @@ class AlarmScreen extends HookWidget {
                     border: Border.all(color: AppColors.amethyst, width: 2),
                   ),
                   child: Text(
-                    context.localize.alarm_time(selectedTime.value.format(context)),
+                    context.localize
+                        .alarm_time(selectedTime.value.format(context)),
                     style: context.theme.textTheme.bodyMedium,
                   ),
                 ),
@@ -118,131 +123,5 @@ class AlarmScreen extends HookWidget {
         ),
       ),
     );
-
   }
-  
 }
-
-// @RoutePage()
-// class AlarmScreen extends StatefulWidget {
-//   const AlarmScreen({super.key});
-
-//   @override
-//   State<StatefulWidget> createState() => AlarmScreenState();
-// }
-
-// class AlarmScreenState extends State<AlarmScreen> {
-//   final alarmCacheRepository = AlarmCacheRepository();
-//   TimeOfDay selectedTime = const TimeOfDay(hour: 8, minute: 0);
-//   bool isAlarmEnabled = false;
-
-//   Future<void> loadAlarmSettings() async {
-//     final alarmSettings = await alarmCacheRepository.loadAlarmSettings();
-
-//     setState(() {
-//       isAlarmEnabled = alarmSettings.isEnabled;
-//       selectedTime = alarmSettings.time;
-//     });
-//   }
-
-//   Future<void> saveAlarmSettings() async {
-//     final alarmSettings =
-//         AlarmSettings(isEnabled: isAlarmEnabled, time: selectedTime);
-//     await alarmCacheRepository.saveAlarmSettings(alarmSettings);
-
-//     if (isAlarmEnabled) {
-//       if (!mounted) return;
-//       await AlarmService.setDailyAlarm(context, selectedTime);
-//     } else {
-//       await AlarmService.stopAlarm();
-//     }
-//   }
-
-//   Future<void> selectTime(BuildContext context) async {
-//     final TimeOfDay? picked = await showTimePicker(
-//       context: context,
-//       initialTime: selectedTime,
-//     );
-
-//     if (picked != null && picked != selectedTime) {
-//       setState(() {
-//         selectedTime = picked;
-//       });
-//     }
-//   }
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     Future.microtask(() async => loadAlarmSettings());
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text(context.localize.alarm_settings)),
-//       floatingActionButton: Padding(
-//         padding: const EdgeInsets.only(
-//           bottom: AppDimensions.paddingMedium,
-//           right: AppDimensions.paddingMedium,
-//         ),
-//         child: FloatingActionButton(
-//           onPressed: () async {
-//             await saveAlarmSettings();
-//             if (!context.mounted) return;
-//             ScaffoldMessenger.of(context).showSnackBar(
-//               SnackBar(content: Text(context.localize.alarm_settings_saved)),
-//             );
-//             context.router.popForced();
-//           },
-//           backgroundColor: AppColors.amethyst,
-//           child: const Icon(Icons.done, color: AppColors.dark),
-//         ),
-//       ),
-//       body: Padding(
-//         padding:
-//             const EdgeInsets.symmetric(horizontal: AppDimensions.paddingBig),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//               children: [
-//                 Text(
-//                   context.localize.enable_alarm,
-//                   style: const TextStyle(fontSize: AppDimensions.fontSizeSmall),
-//                 ),
-//                 Switch(
-//                   value: isAlarmEnabled,
-//                   onChanged: (value) => setState(() => isAlarmEnabled = value),
-//                 ),
-//               ],
-//             ),
-//             if (isAlarmEnabled)
-//               const SizedBox(height: AppDimensions.heightSmall),
-//             if (isAlarmEnabled)
-//               GestureDetector(
-//                 onTap: () async => selectTime(context),
-//                 child: Container(
-//                   padding: const EdgeInsets.symmetric(
-//                     vertical: AppDimensions.paddingMedium,
-//                     horizontal: AppDimensions.paddingBig,
-//                   ),
-//                   decoration: BoxDecoration(
-//                     borderRadius:
-//                         BorderRadius.circular(AppDimensions.borderRadiusMedium),
-//                     border: Border.all(color: AppColors.amethyst, width: 2),
-//                   ),
-//                   child: Text(
-//                     context.localize.alarm_time(selectedTime.format(context)),
-//                     style: context.theme.textTheme.bodyMedium,
-//                   ),
-//                 ),
-//               ),
-//             const SizedBox(height: 4 * AppDimensions.heightHuge),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
