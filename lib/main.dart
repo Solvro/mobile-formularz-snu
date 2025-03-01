765389081037da45200456b8e562d8f6d6771639
24bartixx <jestgitbartix@gmail.com>
2025-03-01 14:54:17 +0100

    chore: portrait orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import "package:alarm/alarm.dart";
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 import "package:flutter_localizations/flutter_localizations.dart";
 import "package:flutter_native_splash/flutter_native_splash.dart";
 import "package:sleep_app/dependency_injection.dart";
@@ -14,6 +15,9 @@ Future<void> main() async {
 
   setupDependencyInjection();
   await Alarm.init();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
 
   FlutterNativeSplash.remove();
 
